850ccff3524054f0ed1c12530e6085aa36c30da7
Paul Cabrera <paul.cabrera.chu@gmail.com>
2025-08-12 22:11:26 -0400

    añadiendo readme

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y 
-// añadirlo a la lista de amigos creada anteriormente.
-
-//let amigoSecreto = generaramigoSecreto();
-
-//function asignarTextoElemento(elemento,text){
-//    let elementoHTML = document.querySelector(elemento);
-//    elementoHTML.innerHTML =text;
-//    return;
-//}
-
-//function campoVacio(){
-    //alert('Por favor, inserte un nombre.' );
-//    let nombreAmigo = document.querySelector('input');
-//    console.log(nombreAmigo === nombreAmigo);
-//    if (nombreAmigo === nombreAmigo){
-//        asignarTextoElemento('p','El nombre del amigo secreto es: ');
-//   }
-//    return;
-//}
-
-//function generaramigoSecreto() {
-    //return Math.floor(Math.random()*5)+1;
-//}
-//asignarTextoElemento('h1','Amigo Secreto!');
-//asignarTextoElemento('p','Por favor, inserte un nombre.')
-
 const inputAmigo = document.getElementById("amigo");
 const listaAmigos = [];
 const ulListAmigos = document.getElementById("listaAmigos");
@@ -35,9 +7,12 @@ function agregarAmigo() {
     //console.log("Hola")
     if (!inputAmigo.value){
         alert("Por favor, inserte un nombre.")
+        return;
     }
+    
     listaAmigos.push(inputAmigo.value);
     ulListAmigos.innerHTML += `<li>${inputAmigo.value}</li>`
+    return;
     
 }
 
@@ -46,3 +21,4 @@ function sortearAmigo() {
     const amigoSecreto = listaAmigos[random];
     ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
 }
+
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 Actividad sobre el reto de realizar un programa en javascript sobre el amigo secreto
 
-![alt text](amigo-secreto\assets\image.png)
+![alt text](C:\Users\HP\OneDrive\Documents\git-y-github\amigo-secreto\assets\image.png)
